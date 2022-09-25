48f450ad7f1d0d4e4a38e1ad85af6cf63d6ff1c6
Al Arafat Siddique <arafatdipto@gmail.com>
2022-09-25 23:52:56 +0600

    ejs templating implemented

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const path= require('path');
-const expressHbs= require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs',expressHbs());
-app.set('view engine','hbs');
+
+app.set('view engine','ejs');
 app.set('views','views');
 
 const adminData= require('./routes/admin');
@@ -19,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminData.routes);
 app.use(shopRouters);
 app.use((req, res, next)=>{
-    res.status(404).render('404', {pageTitle: '404:Not Found'});
+    res.status(404).render('404', {pageTitle: '404:Page Not Found'});
 })
 
 
