4840163349798f6c73b2a4d4f4611d9d5daa65c7
alarafatsiddique <alarafatsiddique@gmail.com>
2022-09-26 10:57:37 +0600

    MVC implemented

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
+//importing controllers
+const productController = require('../controllers/products');
 
 const router = express.Router();
-const adminData = require('./admin');
 
-router.get('/',(req, res, next)=>{
-    console.log('from shop', adminData.products)
-    res.render('shop', {prods: adminData.products, 
-                        pageTitle: 'Products Page',
-                        hasProducts: adminData.products.length>0,
-                        path:'/'})
-})
+
+router.get('/',productController.getProducts);
 
 module.exports= router;
