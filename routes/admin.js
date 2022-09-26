5853f2c050aac1ea11abd31f2c966492521835be
alarafatsiddique <alarafatsiddique@gmail.com>
2022-09-26 15:28:02 +0600

    registering the routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path= require('path');
 //importing controllers
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
+
 
 const router = express.Router();
 
 
-router.get('/add-product', productController.getAddProduct);
-router.post('/add-product',productController.postAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
+router.post('/add-product',adminController.postAddProduct);
 
 module.exports= router;
 
