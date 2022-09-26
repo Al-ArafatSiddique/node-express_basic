8cdcc7b81123f7f8809a8e98d8b31967d357616d
alarafatsiddique <alarafatsiddique@gmail.com>
2022-09-26 11:46:29 +0600

    product model created

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,16 @@
+const Product = require('../models/product');
+
 exports.getAddProduct=(req, res, next)=>{
                                 res.render('add-product', {products: products, 
                                 pageTitle: 'Add Product',
                                 path: '/admin/add-product'})}
-const products= [];
+
 exports.postAddProduct=(req, res, next)=>{
-                        products.push({title: req.body.title});
+                        const product = new Product(req.body.title);
+                        product.save();
                         res.redirect('/')}
 exports.getProducts=(req, res, next)=>{
-  
+    const products= Product.fetchAll();
     res.render('shop', {prods:products, 
                         pageTitle: 'Products Page',
                         hasProducts: products.length>0,
