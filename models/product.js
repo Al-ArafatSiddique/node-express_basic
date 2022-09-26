7827b033226949e87e22af7a6a482703082fbf35
alarafatsiddique <alarafatsiddique@gmail.com>
2022-09-26 16:05:30 +0600

    storing and displaying product data

--- models/product.js
+++ models/product.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
+const p = path.join(__dirname,'../',
   'data',
   'products.json'
 );
@@ -18,8 +17,11 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
