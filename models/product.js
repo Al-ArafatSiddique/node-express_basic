da735c87ad51d29688ac87e65e2de20a46c409cf
Al Arafat Siddique <arafatdipto@gmail.com>
2022-09-28 17:10:43 +0600

    Product deleted from cart on the product deletion

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -53,7 +55,7 @@ module.exports = class Product {
       const updatedProducts = products.filter(prod => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
         if (!err) {
-    
+          Cart.deleteProduct(id, product.price);
         }
       });
     });
