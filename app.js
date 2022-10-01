bbba525e361996b165f542e9eb6358c15a2b998d
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-01 09:11:21 +0600

    creating new user

--- app.js
+++ app.js
@@ -11,17 +11,33 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+//importing models
+const User = require('./models/user');
 
+
+//importing Routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { userInfo } = require('os');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
+
+app.use((req, res, next)=>{
+    User.findById("6337ae2ac46651ba1ded9a44")
+    .then((user)=>{
+        req.user = user;
+        next();
+    }).catch((err)=>{
+        console.log(err);
+        next();
+    })
+})
 
 mongoConnect(()=>{
     app.listen(3000);
