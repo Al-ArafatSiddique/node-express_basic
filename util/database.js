1de51f40c1cbb37cec9543758c9a8aae9bcec197
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-02 13:02:39 +0600

    mongoose is connected

--- util/database.js
+++ util/database.js
@@ -1,27 +1,27 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
 
-let _db;
+// let _db;
 
-const mongoConnect = (callback)=>{
-    MongoClient.connect('mongodb+srv://admin:hq4v1LXQs3u4MGxI@cluster0.qcjc2ek.mongodb.net/shop?retryWrites=true&w=majority')
-    .then(client=>{
-        console.log('connected')
-        _db= client.db();
-        callback();    
-    })
-    .catch(err=>{
-        console.log(err)
-        throw err; 
-    })
-}
+// const mongoConnect = (callback)=>{
+//     MongoClient.connect('mongodb+srv://admin:hq4v1LXQs3u4MGxI@cluster0.qcjc2ek.mongodb.net/shop?retryWrites=true&w=majority')
+//     .then(client=>{
+//         console.log('connected')
+//         _db= client.db();
+//         callback();    
+//     })
+//     .catch(err=>{
+//         console.log(err)
+//         throw err; 
+//     })
+// }
 
-const getDb= ()=>{
-    if(_db){
-        return _db;
-    }
-    throw "No database found";
-}
+// const getDb= ()=>{
+//     if(_db){
+//         return _db;
+//     }
+//     throw "No database found";
+// }
 
-exports.mongoConnect= mongoConnect;
-exports.getDb= getDb;
+// exports.mongoConnect= mongoConnect;
+// exports.getDb= getDb;
