c1e556a33c89ec2b084a06c40f71615e18398266
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-02 11:47:51 +0600

    adding relational order data

--- models/user.js
+++ models/user.js
@@ -83,8 +83,20 @@ class User {
 
   addOrder(){
     const db = getDb();
-    return db.collection('orders')
-    .insertOne(this.cart);
+    return this.getCart()
+    .then(products=>{
+      const order={
+        items: products,
+        user: {
+          _id: new mongodb.ObjectId(this._id),
+          name: this.name
+        }
+      }
+      return db.collection('orders').insertOne(order);
+    }).catch(err=>{
+      console.log(err)
+    })
+ 
   }
 
   static findById(userId) {
