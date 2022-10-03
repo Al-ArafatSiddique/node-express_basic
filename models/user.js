f857dc47deffa8d3721629b4c4700f4c5046ad66
alarafatsiddique <alarafatsiddique@gmail.com>
2022-10-03 12:17:18 +0600

    working on the shopping cart

--- models/user.js
+++ models/user.js
@@ -11,6 +11,29 @@ const userSchema = new Schema({
 
 })
 
+userSchema.methods.addToCart = function(product){
+    const cartProductIndex = this.cart.items.findIndex((cp)=>{
+              return cp.productId.toString() === product._id.toString();
+              // return cp.productId == product._id; this also works
+            })
+            let newQuantity= 1;
+            const updatedCartItems = [...this.cart.items];
+            if(cartProductIndex >= 0){
+              console.log(this.cart.items[cartProductIndex].quantity);
+                newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+                updatedCartItems[cartProductIndex].quantity = newQuantity;
+            }else{
+              updatedCartItems.push({
+                productId: product._id,
+                 quantity: newQuantity});
+            }
+          
+            const updatedCart = {items: updatedCartItems};
+            this.cart= updatedCart;
+            return this.save();
+              
+}
+
 module.exports= mongoose.model('User', userSchema);
 
 
