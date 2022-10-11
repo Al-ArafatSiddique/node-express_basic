9ab5b19c9830356ba3f202e4ef7fc397ac33d4c8
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-11 17:26:57 +0600

    error solved

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,19 +40,21 @@ exports.postSignup = (req, res, next) => {
     if(userDoc){
       return res.redirect('/signup');
     }
-    return bcrypt.hash(password, 12);
-  })
-  .then(hashPassword=>{
-    const user = new User({
-      email: email,
-      password: hashPassword,
-      cart: {items: []}
+    return bcrypt
+        .hash(password, 12)
+      .then(hashPassword=>{
+      const user = new User({
+        email: email,
+        password: hashPassword,
+        cart: {items: []}
+      })
+      return user.save();
     })
-    return user.save();
+    .then(result=>{
+      res.redirect('/login');
+    })
   })
-  .then(result=>{
-    res.redirect('/login');
-  })
+
   .catch(err=>{
     console.log(err);
   })
