6cc07c01920d1f6847a385499eb41db44780ba6e
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-11 15:04:14 +0600

    project setup for authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,39 @@
 const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    //  const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
-    //console.log(req.get('Cookie').split(';')[1].split('=')[1]);
-    //console.log(req.session.isLoggedIn);
-    res.render('auth/login', {
-      pageTitle: 'Login',
-      path: '/login',
-      isAuthenticated: req.session.isLoggedIn,
-    });
-  };
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
 
-  exports.postLogin = (req, res, next) => {
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
 
-    User.findById('633a71140870f9ab38969e31')
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
-      req.session.isLoggedIn= true;
+      req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(err=>{
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
-      })
-      
+      });
     })
     .catch(err => console.log(err));
+};
 
-  };
+exports.postSignup = (req, res, next) => {};
 
-  exports.postLogout = (req, res, next) => {
-    req.session.destroy(err=>{
-      console.log(err);
-    })
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
     res.redirect('/');
-  };
+  });
+};
