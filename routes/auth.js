992e25333096db7cb0f0ccc09d303cc7b5da91cc
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-13 11:48:17 +0600

    confirm password should match

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,14 @@ router.post('/login', authController.postLogin);
 
 router.post('/signup', 
 [check('email').isEmail().withMessage('Pls enter a valid email address')
-,body('password', 'Pls enter a pass of at least 5 characters and with num and text').isLength({min:5}).isAlphanumeric()]
+,body('password', 'Pls enter a pass of at least 5 characters and with num and text').isLength({min:5}).isAlphanumeric()
+,body('confirmPassword').custom((value, {req})=>{
+    if(value!==req.body.password){
+        throw new Error('Password do not match');
+    }
+    return true;
+})
+]
 , authController.postSignup);
 
 router.post('/logout', authController.postLogout);
